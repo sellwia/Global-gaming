b597cac619dfa9042968189b0e380070b894e068
sellwia <Sellydwikurnia@gmail.com>
2026-01-01 18:50:11 +0700

    buat body pada splashscreen 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_global_gaming/screen/splashscreen1.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const SplashScreen3(),
+      home: const SplashScreen1(),
     );
   }
 }
---
+++ lib/screen/splashscreen1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen1 extends StatelessWidget {
+  const SplashScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple.shade900,
+              Colors.deepPurple.shade600,
+              Colors.purple.shade500,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
