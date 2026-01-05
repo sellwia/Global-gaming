e85ae3fea139a20473848b0263d16fb6efcda987
I-JasonLee <joshuajasonlee@gmail.com>
2026-01-06 03:17:29 +0700

    Initialize Firebase and add firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:project_global_gaming/screen/splashscreen1.dart';
 
 void main() {
