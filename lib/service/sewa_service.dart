c17be1277281199ddfbcc0207d35cc4de67dfd46
I-JasonLee <joshuajasonlee@gmail.com>
2026-01-06 04:04:28 +0700

    Tambah class SewaService

--- lib/service/sewa_service.dart
+++ lib/service/sewa_service.dart
@@ -1,1 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SewaService {
+  static Future<void> simpanSewa({
+    required String namaUser,
+    required String ps,
+    required int jam,
+    required int hargaJam,
+    required int total,
+  }) async {
+    await FirebaseFirestore.instance.collection('sewa').add({
+      'namaUser': namaUser,
+      'ps': ps,
+      'jam': jam,
+      'hargaJam': hargaJam,
+      'total': total,
+      'createdAt': Timestamp.now(),
+    });
+  }
+}
