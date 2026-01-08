ee874fdb0bf2a5a8f7c3981fdecc2d99caf5b091
Krisno-Gatsusan <kgatsusan@gmail.com>
2026-01-08 21:21:01 +0700

    pembuatan card untuk profil team

--- lib/profil_team.dart
+++ lib/profil_team.dart
@@ -22,6 +22,55 @@ class TeamPage extends StatelessWidget {
             ],
           ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: GridView.count(
+            crossAxisCount: 2, // 2 kolom
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            childAspectRatio: 0.78,
+            children: const [
+              TeamBox(
+                nama: 'Andi Pratama',
+                nim: '22123456',
+                jabatan: 'Admin',
+                image: '../assets/images/sp1.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TeamBox extends StatelessWidget {
+  final String nama;
+  final String nim;
+  final String jabatan;
+  final String image;
+
+  const TeamBox({
+    super.key,
+    required this.nama,
+    required this.nim,
+    required this.jabatan,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withValues(alpha: 38),
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 20),
+            blurRadius: 6,
+            offset: const Offset(0, 4),
+          ),
+        ],
       ),
     );
   }
